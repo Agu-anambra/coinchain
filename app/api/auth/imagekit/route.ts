df559a5aac50f8f38c5c60d139b639154244f893
Wenceslaus Obimka <obimkasomto@gmail.com>
2025-06-23 23:42:16 +0100

    Update route.ts
    check something out

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -10,44 +10,8 @@ const imagekit = new ImageKit({
   urlEndpoint,
 });
 
-// Allowlist both domains
-const allowedOrigins = [
-  "https://www.coinchain.tech",
-  "https://coinchain.tech",
-];
+export async function GET() {
 
-function getCorsOrigin(request: Request) {
-  const origin = request.headers.get("origin");
-  return allowedOrigins.includes(origin || "") ? origin : null;
-}
+    return NextResponse.json(imagekit.getAuthenticationParameters())
 
-export async function GET(request: Request) {
-  console.log("Incoming origin:", request.headers.get("origin"));
-
-  const origin = getCorsOrigin(request);
-  const res = NextResponse.json(imagekit.getAuthenticationParameters());
-
-  if (origin) {
-    res.headers.set("Access-Control-Allow-Origin", origin);
-  }
-
-  res.headers.set("Access-Control-Allow-Methods", "GET");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type");
-
-    return res;
-
-}
-
-export async function OPTIONS(request: Request) {
-  const origin = getCorsOrigin(request);
-  const res = new NextResponse(null, { status: 204 });
-
-  if (origin) {
-    res.headers.set("Access-Control-Allow-Origin", origin);
-  }
-
-  res.headers.set("Access-Control-Allow-Methods", "GET");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type");
-
-  return res;
 }
